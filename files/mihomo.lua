92b6f561fed470abe548fbc3d227fea4ed53f571
Jahid Hasan Shuvo <130297130+jahid421@users.noreply.github.com>
2026-08-21 23:25:33 +0600

    Create mihomo.lua

---
+++ files/mihomo.lua
@@ -0,0 +1,5 @@
+module("luci.controller.mihomo", package.seeall)
+
+function index()
+    entry({"admin", "services", "mihomo"}, template("mihomo/main"), _("UzumakiClash 🌀"), 60).dependent = false
+end
