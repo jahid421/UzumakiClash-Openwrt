2d5788361178e66f6c6cebe6c4c21418325b2087
Jahid Hasan Shuvo <130297130+jahid421@users.noreply.github.com>
2026-08-31 00:07:28 +0600

    Update mihomo.lua

--- files/mihomo.lua
+++ files/mihomo.lua
@@ -1,5 +1,10 @@
 module("luci.controller.mihomo", package.seeall)
 
 function index()
-    entry({"admin", "services", "mihomo"}, template("mihomo/main"), _("UzumakiClash 🌀"), 60).dependent = false
+    entry(
+        {"admin", "services", "mihomo"},
+        template("mihomo/main"),
+        _("UzumakiClash 🌀"),
+        60
+    ).dependent = false
 end
